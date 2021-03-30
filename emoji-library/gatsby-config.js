214147ca3dd8747ebe3d6852e7a327e9b841383f
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-03-30 11:14:35 +0100

    feat: adding bootstrap css

--- emoji-library/gatsby-config.js
+++ emoji-library/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -20,7 +20,7 @@ module.exports = {
       options: {
         name: `emojiData`,
         path: `${__dirname}/src/data/`,
-      }
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
