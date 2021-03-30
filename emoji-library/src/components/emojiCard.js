92e27d218c66a33c65f2590335de2364e05dabcf
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-03-30 11:13:42 +0100

    feat: Ui layout

--- emoji-library/src/components/emojiCard.js
+++ emoji-library/src/components/emojiCard.js
@@ -3,7 +3,18 @@ import React from "react"
 const EmojiCard = ({ emoji, annotation }) => {
   return (
     <>
-      <div className="col-sm">One of three columns</div>
+      <div className="col-sm">
+        <div className="card-wrapper">
+          <header className="emoji--image">
+            <button>A</button>
+          </header>
+          <div className="code--text">
+            <button>
+              <code>:T:</code>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
