2d73b5db0c3ba0a888d2fbe7fc7e87c60ccc8a6f
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-03-30 08:42:21 +0100

    feat: emoji library layout

--- emoji-library/src/pages/index.js
+++ emoji-library/src/pages/index.js
@@ -8,9 +8,20 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="container border">
+    <div className="container">
       <div className="row text-center">
         <div className="col-xs-12">One of three columns</div>
+        <div className="col-sm">
+          <div
+            className=""
+            style={{
+              border: `1px solid blue`,
+              padding: `0 15px`,
+            }}
+          >
+            One of three columns
+          </div>
+        </div>
         <div className="col-sm">One of three columns</div>
         <div className="col-sm">One of three columns</div>
         <div className="col-sm">One of three columns</div>
