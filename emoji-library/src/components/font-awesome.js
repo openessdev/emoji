129b353b546ebaaafecaa41359ed2b618e204501
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-04-02 20:58:13 +0100

    feat: Implementing fontawesome icon component

---
+++ emoji-library/src/components/font-awesome.js
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core"
+import {
+  faStar,
+  faDove,
+  faThList,
+  faTh,
+} from "@fortawesome/free-solid-svg-icons"
+import { fab, faTwitter } from "@fortawesome/free-brands-svg-icons"
+
+library.add(fab, faStar, faTwitter, faDove, faThList, faTh)
