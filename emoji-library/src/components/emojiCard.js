926a704dbe419a6a21c656600fb721996148a072
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-04-02 20:51:33 +0100

    feat: component logic

--- emoji-library/src/components/emojiCard.js
+++ emoji-library/src/components/emojiCard.js
@@ -1,17 +1,50 @@
 import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Clipboard from "react-clipboard.js"
+import generateLightColorHex from "../helpers/util"
 
-const EmojiCard = ({ emoji, annotation }) => {
+const EmojiCard = ({ item, column }) => {
+  const image = getImage(item.openess_images.color.png)
+
   return (
     <>
-      <div className="col-sm">
+      <div
+        className={`col-sm-6 col-md-4 ${
+          column === false ? "col-lg-3" : "col-lg-4"
+        }`}
+      >
         <div className="card-wrapper">
-          <header className="emoji--image">
-            <button>A</button>
-          </header>
-          <div className="code--text">
-            <button>
-              <code>:T:</code>
-            </button>
+          <div className={`${column === false ? false : "horizontal__layout"}`}>
+            <div
+              className={`emoji--image ${
+                column === false ? false : "emoji--image__modified"
+              }`}
+              style={{
+                backgroundColor: `${generateLightColorHex()}`,
+              }}
+            >
+              <span>
+                <GatsbyImage
+                  image={image}
+                  alt={item.code}
+                  style={{
+                    width: `${column === false ? false : "50%"}`,
+                  }}
+                />
+              </span>
+            </div>
+            <div
+              className={`code--text text-white px-1 ${
+                column === false ? false : "code--text__modified "
+              }`}
+            >
+              <span className="">
+                <Clipboard data-clipboard-text={item.code}>
+                  {item.code}
+                </Clipboard>
+                <div className="description">{item.name}</div>
+              </span>
+            </div>
           </div>
         </div>
       </div>
